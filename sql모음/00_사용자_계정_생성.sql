6babc92c5fdad6a526f679e579bb29db5e39ae80
limbit95 <limbit@naver.com>
2024-03-27 18:07:53 +0900

    학원

--- sql모음/00_사용자_계정_생성.sql
+++ sql모음/00_사용자_계정_생성.sql
@@ -8,10 +8,10 @@ ALTER SESSION SET "_ORACLE_SCRIPT" = TRUE;
 -- CTRL + ENTER : 선택한 SQL 수행
 
 -- 사용자 계정 생성
-CREATE USER zoo IDENTIFIED BY zoo;
+CREATE USER warrioronline IDENTIFIED BY warrioronline;
 
 -- 사용자 계정에 권한 부여
-GRANT RESOURCE, CONNECT TO zoo;
+GRANT RESOURCE, CONNECT TO warrioronline;
 
 -- 객체가 생성될 수 있는 공간 할당량 지정
 ALTER USER zoo DEFAULT TABLESPACE SYSTEM QUOTA UNLIMITED ON SYSTEM;
